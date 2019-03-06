9c5d694ca9e7d0c14980208392aa5f5669213d86
liuguoqing@sxwdsoft.com <liuguoqing@sxwdsoft.com>
2019-03-06 15:42:34 +0800

    首次提交eureka项目代码

---
+++ src/main/java/com/example/demo/FcsEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcsEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcsEurekaApplication.class, args);
+	}
+
+}
